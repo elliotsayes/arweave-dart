ab3eb68a6777480ee716302db6f90485b05c131e
Steven Yi <stevenyi@gmail.com>
2023-05-15 17:11:02 -0400

    Removed commented out test per code review

--- test/crypto/hmac_drbg_secure_random_test.dart
+++ test/crypto/hmac_drbg_secure_random_test.dart
@@ -6,28 +6,6 @@ import 'package:pointycastle/export.dart';
 import 'package:test/test.dart';
 
 void main() {
-  // test('HmacDrbgSecureRandom returns same type data as FortunaRandom',
-  //     () async {
-  //   final seed = Uint8List.fromList(List.generate(64, (i) => 0));
-  //   final secureRandom = HmacDrbgSecureRandom();
-  //   secureRandom.seed(KeyParameter(Uint8List.fromList(seed)));
-
-  //   // TODO: figure out why bit length appears to be 2047 or 2048 depending on
-  //   // seed
-  //   final seedFortuna = Uint8List.fromList(List.generate(32, (i) => 1));
-  //   final fortunaRandom = FortunaRandom();
-  //   fortunaRandom.seed(KeyParameter(Uint8List.fromList(seedFortuna)));
-
-  //   final bigIntHmac = secureRandom.nextBigInteger(2048);
-  //   final bigIntFortuna = fortunaRandom.nextBigInteger(2048);
-
-  //   expect(bigIntFortuna.bitLength, equals(2048));
-  //   expect(bigIntHmac.bitLength, equals(2048));
-  //   expect(bigIntHmac.bitLength, equals(bigIntFortuna.bitLength));
-  // }, onPlatform: {
-  //   'browser': Skip('dart:io unavailable'),
-  // });
-
   test('HmacDrbgSecureRandom returns same bytes from 1-filled seed as JS',
       () async {
     final seed = Uint8List.fromList(List<int>.generate(64, (i) => 1));
